3510765e39f04dc1ae814a7088b55b69458fb3ac
aneeshamanocha <32530260+aneeshamanocha@users.noreply.github.com>
2017-10-23 11:23:02 -0400

    Update finalproject.c

--- finalproject.c
+++ finalproject.c
@@ -4,32 +4,63 @@
 #define CARDS 52
 #define SUITS 4
 #define FACES 13
+#define CAPACITY 1
 
+//each card composes of a suit & face
 typedef struct card {
-  int cardNum;
-  char *suit;
-  char *face;
+  int cardNum; //0-51
+  char *suit; //diamonds, clubs, hearts, spades
+  int face; //2-14
 } Card;
 
 typedef struct vector {
   size_t size;
   size_t capacity;
   Card *cards;
-}
+} Vector;
 
-
-void printCard(const int card, const char *suits[], const char *faces[]);
-void resetDeck(int deck[]);
-void shuffleDeck(int deck[]);
-/*
-* functions:
-* shuffle (Fisher Yates)
-*/
+void clear(Vector *vector);
+void init(Vector *vector);
+void resetDeck(Card * deck[], const char *suits[], int faces[]);
+void shuffleDeck(Card deck[]);
 
 int main(void){
-  
-  //5 arraylists: player, comp1, comp2, comp3, pile
+  //start the game
+  //srand(time(NULL));
   puts("Welcome to the Game of BS! Are you ready to play?");
+  char str1;
+  printf("y or n?\n");
+  scanf("%c", &str1);
+  if(str1 == 'n') {
+    printf("Maybe next time!\n");
+    return 0;
+  }
+
+  //5 arraylists: player, comp1, comp2, comp3, pile & initialize all of them
+  //1 card deck - initalize it as well
+  Vector player;
+  Vector comp1;
+  Vector comp2;
+  Vector comp3;
+  Vector pile;
+  Card deck[CARDS];
+  const char *suits[SUITS] = {"Diamonds", "Clubs", "Hearts", "Spades"};
+  int faces[FACES] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
+  init(&player);
+  init(&comp1);
+  init(&comp2);
+  init(&comp3);
+  init(&pile);
+  resetDeck(&deck, suits, faces);
+  for(size_t i = 0; i < CARDS; i++) {
+    printf("CardNum: %d Suit: %s Face: %d\n", deck[i].cardNum, deck[i].suit, deck[i].face);
+  }
+  puts("---------------");
+  shuffleDeck(deck);
+  for(size_t i = 0; i < CARDS; i++) {
+    printf("CardNum: %d Suit: %s Face: %d\n", deck[i].cardNum, deck[i].suit, deck[i].face);
+  }
+
   //shuffle
   //assign cards to three computers and player
   //show card to player & ask when ready to start Game
@@ -40,38 +71,39 @@ int main(void){
     //each user has random chance to call BS
       //if BS is called, check the last card
         //if last card is the incorrect card, add all the cards from pile into arraylist & reset pile to 0
-    
+
   //if comp1/comp2/comp3/player size == 0, print ("comp/player won the game!")
-  srand(time(NULL));
-  
-  int deck[CARDS] = {0};
-  const char *suits[SUITS] = {"Diamonds", "Clubs", "Hearts", "Spades"};
-  const char *faces[FACES] = {"Deuce", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King", "Ace"};
-  
-  resetDeck(deck);
-  shuffleDeck(deck);
-  
+
+
+
   return 0;
 }
 
-void printCard(const int card, const int char *suits[], const char *faces[])
-{
-  printf("%s of %s\n", faces[card%FACES], suits[card/FACES]);
+void clear(Vector *vector) {
+    free(vector->cards);
 }
 
-void resetDeck(int deck[])
-{
-  for(size_t i =0; i < CARDS; ++i)
-  deck[i] = (int)i;
+void init(Vector *vector) {
+  vector->size = 0;
+  vector->capacity = CAPACITY;
+  vector->cards = calloc(CAPACITY, sizeof(char));
 }
 
-void shuffleDeck(int deck[])
-{
-  int randIndex, tmp;
-  for(size_t i =0; i < CARDS-1; ++i)
+void resetDeck(Card * deck[], const char *suits[], int faces[]) {
+  for(size_t i = 0; i < CARDS; ++i) {
+    deck[i]->cardNum = i;
+    deck[i]->suit = suits[i/SUITS];
+    deck[i]->face = faces[i % FACES];
+  }
+}
+
+void shuffleDeck(Card deck[]) {
+  Card tmp;
+  int randIndex;
+  for(size_t i = 0; i < CARDS-1; ++i)
   {
     randIndex = rand() % (CARDS-i) + i;
-    int tmp = deck[i];
+    Card tmp = deck[i];
     deck[i] = deck[randIndex];
     deck[randIndex] = tmp;
   }
