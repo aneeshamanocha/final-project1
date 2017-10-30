687099c368306728ed603884e1108202e92604ec
aneeshamanocha <32530260+aneeshamanocha@users.noreply.github.com>
2017-10-30 15:09:20 -0400

    Update finalproject.c

--- finalproject.c
+++ finalproject.c
@@ -12,9 +12,10 @@
 typedef struct card {
   int cardNum; //0-51
   char *suit; //diamonds, clubs, hearts, spades
-  char *face; //2-14
+  int face; //2-14
 } Card;
 
+//arraylist
 typedef struct vector {
   size_t size; //size of specific vector
   size_t capacity; //capacity of vector
@@ -23,14 +24,14 @@ typedef struct vector {
 
 void clear(Vector *vector); //free vector
 void init(Vector *vector); //initialize vector
-void resetDeck(Card deck[], char *suits[], char *faces[]); //reset deck of cards
+void resetDeck(Card deck[], char *suits[], int faces[]); //reset deck of cards
 void shuffleDeck(Card deck[]); //shuffle deck of cards
 void printDeck(Card deck[]); //print deck
-void printVector(Vector *vector);
+void printVector(Vector *vector); //print vector
 void insertCard(Vector *vector, Card card); //insert cards for each player
 void resizeIfFull(Vector *vector); //resize vector if full
 void deleteCard(Vector *vector, int cardNum); //deletes card
-Vector * findAceOfSpades(Vector *vector); //find ace of spades
+int findAceOfSpades(Vector *vector); //find ace of spades
 
 int main(void){
   //start the game
@@ -56,8 +57,7 @@ int main(void){
   Vector pile;
   Card deck[CARDS];
   char *suits[SUITS] = {"Diamonds", "Clubs", "Hearts", "Spades"};
-  char *faces[FACES] = {"Two", "Three", "Four", "Five", "Six", "Seven", "Eight",
-  "Nine", "Ten", "Jack", "Queen", "King", "Ace"};
+  int faces[FACES] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
   init(&player);
   init(&comp1);
   init(&comp2);
@@ -67,7 +67,7 @@ int main(void){
   shuffleDeck(deck);
 
   //explaining rules of BS
-  puts("Here are the rules for BS: The cards are distributed evenly between three computers players and you. The ace of spades will be placed down first and then following the placement, the next player will check their deck for the number two. If they have any twos, they will place all of them down and say they have an amount of twos. If they do not have a two, they will place down one random card and claim they placed down a two. Each player then has a chance of calling BS on them. If somebody gets called for BS, the last card placed on the pile will be shown to the players. If the person who placed down the last card lied and the card wasn't the number they stated it was, the whole pile gets added to their deck. If the person who called BS is wrong and the person does have that card, they will get the whole pile added to their deck. The game will circle through the players, and the next player will place down threes. The same procedure will continue to occur until somebody runs out of cards.");
+  puts("Here are the rules for BS: The cards are distributed evenly between three computers players and you. The ace of spades will be placed down first and then following the placement, the next player will check their deck for the number two. If they have any twos, they will place all of them down and say they have an amount of twos. If they do not have a two, they will place down one random card and claim they placed down a two. Each player then has a chance of calling BS on them. If somebody gets called for BS, the last card placed on the pile will be shown to the players. If the person who placed down the last card lied and the card wasn't the number they stated it was, the whole pile gets added to their hand. If the person who called BS is wrong and the person does have that card, they will get the whole pile added to their deck. The game will circle through the players, and the next player will place down threes. The same procedure will continue to occur until somebody runs out of cards.");
   puts("");
 
   sleep(5);
@@ -85,21 +85,21 @@ int main(void){
   puts("FOR COMP 1");
   for(size_t i = 0; i < 13; ++i) {
     insertCard(&comp1, deck[i+13]);
-    printf("CardNum: %d Suit: %s Face: %s\n", (comp1.cards[i]).cardNum, (comp1.cards[i]).suit, (comp1.cards[i]).face);
+    printf("CardNum: %d Suit: %s Face: %d\n", (comp1.cards[i]).cardNum, (comp1.cards[i]).suit, (comp1.cards[i]).face);
   }
 
   //comp2 assigned cards
   puts("FOR COMP 2");
   for(size_t i = 0; i < 13; ++i) {
     insertCard(&comp2, deck[i+26]);
-    printf("CardNum: %d Suit: %s Face: %s\n", (comp2.cards[i]).cardNum, (comp2.cards[i]).suit, (comp2.cards[i]).face);
+    printf("CardNum: %d Suit: %s Face: %d\n", (comp2.cards[i]).cardNum, (comp2.cards[i]).suit, (comp2.cards[i]).face);
   }
 
   //comp3 assigned cards
   puts("FOR COMP 3");
   for(size_t i = 0; i < 13; ++i) {
     insertCard(&comp3, deck[i+39]);
-    printf("CardNum: %d Suit: %s Face: %s\n", (comp3.cards[i]).cardNum, (comp3.cards[i]).suit, (comp3.cards[i]).face);
+    printf("CardNum: %d Suit: %s Face: %d\n", (comp3.cards[i]).cardNum, (comp3.cards[i]).suit, (comp3.cards[i]).face);
   }
 
   /*Vector tmp;
@@ -136,11 +136,11 @@ int main(void){
     }
   }*/
 
+  Vector tmp;
   //find ace of spades and add to pile
-  if(findAceOfSpades(&player) != NULL) {
+  if(findAceOfSpades(&player) == 1) {
     puts("hello1");
     for(size_t i = 0; i <= player.size; ++i) {
-      puts("??");
       if((player.cards[i]).cardNum == 51) {
         insertCard(&pile, player.cards[i]);
         deleteCard(&player, (player.cards[i]).cardNum);
@@ -148,38 +148,93 @@ int main(void){
         break;
       }
     }
-  } else if(findAceOfSpades(&comp1) != NULL) {
-    puts("hello3");
+    tmp = player;
+  } else if(findAceOfSpades(&comp1) == 1) {
+    puts("hello2");
     for(size_t i = 0; i <= comp1.size; ++i) {
-      puts("??");
       if((comp1.cards[i]).cardNum == 51) {
-        puts("hello4");
         insertCard(&pile, comp1.cards[i]);
         deleteCard(&comp1, (comp1.cards[i]).cardNum);
         puts("in comp 1");
         break;
       }
     }
-  } else if(findAceOfSpades(&comp2) != NULL) {
-    puts("hello5");
+    tmp = comp1;
+  } else if(findAceOfSpades(&comp2) == 1) {
+    puts("hello3");
     for(size_t i = 0; i <= comp2.size; ++i) {
-      puts("??");
       if((comp2.cards[i]).cardNum == 51) {
-        puts("hello6");
         insertCard(&pile, comp2.cards[i]);
         deleteCard(&comp2, (comp2.cards[i]).cardNum);
         puts("in comp 2");
         break;
       }
     }
+    tmp = comp2;
   } else {
     for(size_t i = 0; i <= comp3.size; ++i) {
+      puts("hello4");
       if((comp3.cards[i]).cardNum == 51) {
         insertCard(&pile, comp3.cards[i]);
         deleteCard(&comp3, (comp3.cards[i]).cardNum);
         puts("in comp 3");
         break;
       }
+    }
+    tmp = comp3;
+  }
+
+  int currentCard = 2;
+  char str2, str3;
+
+  while((player.size != 0) && (comp1.size != 0) && (comp2.size != 0) && (comp3.size != 0)) {
+    //find next player
+    if((tmp.cards[0]).cardNum == (player.cards[0]).cardNum) {
+      tmp = comp1;
+    } else if((tmp.cards[0]).cardNum == (comp1.cards[0]).cardNum) {
+      tmp = comp2;
+    } else if((tmp.cards[0]).cardNum == (comp2.cards[0]).cardNum) {
+      tmp = comp3;
+    } else {
+      tmp = player;
+
+      //check player is ready to play
+      puts("It's your turn. Are you ready to play?");
+      printf("y or n?\n");
+      scanf("%c", &str2);
+      while(str2 == 'n') {
+        puts("Do you want to stop the game?");
+        printf("y or n?\n");
+        scanf("%c", &str3);
+        if(str2 == 'y') {
+          puts("Maybe next time!");
+          return 0;
+        }
+        puts("Let's try again. It's your turn. Are you ready to play?");
+        printf("y or n?\n");
+        scanf("%c", &str2);
+      }
+
+      //find the current card
+      puts("Here's your deck of cards");
+      sleep(2);
+      printVector(&player);
+
+    }
+
+    break;
+
+    //find the next person to put down the card
+    //check their pile of cards for that face
+    //if they don't have those card, place down a random card (remove random card from hand)
+    //if they do have the card(s), place all of them down (remove card(s) from hand)
+    //each player afterwards has a random chance of calling BS
+
+    //changes the current card in play
+    if(currentCard == 14) {
+      currentCard = 2;
+    } else {
+      currentCard++;
     }
   }
 
@@ -211,7 +266,7 @@ void init(Vector *vector) {
   vector->cards = calloc(CAPACITY, sizeof(char));
 }
 
-void resetDeck(Card deck[], char *suits[], char *faces[]) {
+void resetDeck(Card deck[], char *suits[], int faces[]) {
   for(size_t i = 0; i < CARDS; ++i) {
     deck[i].cardNum = i;
     deck[i].suit = suits[i/FACES];
@@ -232,13 +287,13 @@ void shuffleDeck(Card deck[]) {
 
 void printVector(Vector *vector) {
   for(size_t i = 0; i < vector->size; ++i) {
-    printf("Card %lu: %s of %s\n", (i+1), (vector->cards[i]).face, (vector->cards[i]).suit);
+    printf("Card %lu: %d of %s\n", (i+1), (vector->cards[i]).face, (vector->cards[i]).suit);
   }
 }
 
 void printDeck(Card deck[]) {
   for(size_t i = 0; i < CARDS; i++) {
-    printf("CardNum: %d Suit: %s Face: %s\n", deck[i].cardNum, deck[i].suit, deck[i].face);
+    printf("CardNum: %d Suit: %s Face: %d\n", deck[i].cardNum, deck[i].suit, deck[i].face);
   }
 }
 
@@ -255,13 +310,13 @@ void resizeIfFull(Vector *vector) {
   }
 }
 
-Vector * findAceOfSpades(Vector *vector){
+int findAceOfSpades(Vector *vector){
   for(size_t i = 0; i < vector->size; ++i) {
     if((vector->cards[i]).cardNum == 51) {
-      return vector;
+      return 1;
     }
   }
-  return NULL;
+  return 0;
 }
 
 void deleteCard(Vector *vector, int cardNum){
